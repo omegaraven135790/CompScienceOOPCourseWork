39a75f279fdc75f5950f8a2edafd534007215010
Zikrith <akmalharith463@gmail.com>
2025-12-03 12:27:17 +0000

    made a workable version of main and base class just fixed some kinks and questionable designs

--- GroupAssignment/src/OOPgroupassignment/HealthRecord.java
+++ GroupAssignment/src/OOPgroupassignment/HealthRecord.java
@@ -3,50 +3,77 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package OOPgroupassignment;
-import java.util.Scanner;
 //base class needs local date im just unsure how to implement it 
 import java.time.LocalDate;
-import java.util.ArrayList;
 /**
  *
  * @author Akmal Zikry
  * @author doshyant
  */
 public class HealthRecord {
-    protected String name;
-    protected int age;
-    protected double weight;
-    protected double height;
+    private String name;
+    private int age;
+    private double weight;
+    private double height;
+    private LocalDate date;
+    private int calorie;
+    private int meals;
+    private int waterDrunk;
+    private int steps;
     
-
+    public int getCalorie(){return this.calorie;}
+    public void setCalorie(int calorie){this.calorie = calorie;}
     
-    public void HealthRecord(HealthTrackerApp menu){
-        Scanner input = new Scanner (System.in);
-        ActivityRecord activitygoal = new ActivityRecord();
-        DietRecord dietgoal = new DietRecord();
-        this.name = menu.getName();
-        this.age = menu.getAge();
-        this.weight = menu.getWeight();
-        this.height = menu.getHeight();
+    public int getMeals(){return this.meals;}
+    public void setMeals(int meals){this.meals = meals;}
+    
+    public int getWater(){return this.waterDrunk;}
+    public void setWater(int waterDrunk){this.waterDrunk = waterDrunk;}
+    
+    public int getSteps(){return this.steps;}
+    public void setSteps(int steps){this.steps = steps;}
+    
+    public String getName(){return this.name;}
+    public void setName(String name){this.name = name;}
+    
+    public int getAge(){return this.age;}
+    public void setAge(int age){this.age = age;}
+    
+    public double getWeight(){return this.weight;}
+    public void setWeight(double weight){this.weight = weight;}
+    
+    public double getHeight(){return this.height;}
+    public void setHeight(double height){this.height = height;}
+    
+    public HealthRecord(){
+    this.name = "";
+    this.age = 0;
+    this.weight = 0.0;
+    this.height = 0.0;
+}
+    
+    public HealthRecord(String name, int age, double weight, double height){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;      
+    }
+    
+    public void displayAccountInfo() {
+        System.out.println("Username: " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Weight: " + this.weight + " kg");
+        System.out.println("Height: " + this.height + " m");
+    }
+    
+    public String getDate(){
+        LocalDate date = LocalDate.now();
+        int day = date.getDayOfMonth();
+        int month = date.getMonthValue();
+        int year = date.getYear();
         
-        System.out.print("Welcome to your Health Record, what kind of record would you like to access?");
-        System.out.print("\n Press 1 to view your Activity Record \n Press 2 to view Diet Record \n Press 3 to log out");
-        int recordChoice = input.nextInt();
-         
-        while(recordChoice != 3){
-            switch(recordChoice){
-                case 1:
-                    System.out.print("Your Activity Record: ");
-                    break;
-                
-                case 2:
-                    System.out.print("Your Diet Record: ");
-                    break;
-                    
-                default:
-                    System.out.print("IVALID INPUT!");
-                    break;
-            }
-        }
+        String formatDate = day + "-" + month + "-" + year;
+        
+        return formatDate;
     }
 }
